ca4d768579230a19d75a4ba727da5b985ebe4361
Mike Morrow <mmorrow@vt.edu>
2016-05-26 21:11:11 -0400

    add iptables save to allow reboot

--- scripts/installOpenstack.sh
+++ scripts/installOpenstack.sh
@@ -18,3 +18,4 @@ sudo -u stack "/vagrant/scripts/installOpenstack.stack"
 # ---------------------------------------------------------------------------
 iptables -I INPUT 1 -p tcp -m tcp --dport 80 -j ACCEPT
 iptables -I INPUT 1 -p tcp -m tcp --dport 8080 -j ACCEPT
+/usr/libexec/iptables/iptables.init save
